53fbaac3213db39973ef5b83464542a7f26e9b36
Dustin McDowell <dustinmcdowell1989@gmail.com>
2018-07-18 17:40:02 -0500

    styling single post page

--- client/components/posts/post.js
+++ client/components/posts/post.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Image, Container, Card } from 'semantic-ui-react'
 import { getPostThunk } from '../../store'
 
 class Post extends React.Component {
@@ -9,17 +10,27 @@ class Post extends React.Component {
   render() {
     const { post } = this.props
     return (
-      <div>
-        <h1>{post.title}</h1>
-        <img src={post.imageUrl} />
-        <h3>{post.description}</h3>
-        <h3>{`$${post.price}`}</h3>
-        {post.user && (
-          <p>
-            <em>Posted by {post.user.name}</em>
-          </p>
-        )}
-      </div>
+      <Container>
+        <Card>
+          <Image src={post.imageUrl} size="large" fluid />
+          <Card.Content>
+            <Card.Header>{post.title}</Card.Header>
+            <Card.Meta>
+              {post.user && (
+                <p>
+                  <em>Posted by {post.user.name}</em>
+                </p>
+              )}
+            </Card.Meta>
+            <Card.Description>
+              <h3>{post.description}</h3>
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <h3>{`$${post.price}`}</h3>
+          </Card.Content>
+        </Card>
+      </Container>
     )
   }
 }
