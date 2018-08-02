150593ecc6a9a80de229a08ea1af87e51f89cff8
Dustin McDowell <dustinmcdowell1989@gmail.com>
2018-08-01 21:10:03 -0500

    clean up nav bar

--- client/user-nav.js
+++ client/user-nav.js
@@ -12,37 +12,43 @@ const UserContainer = styled.ul`
 `
 
 const UserNav = props => {
-  const isLoggedIn = props.currentUser
+  const isLoggedIn = !!props.currentUser.id
   return (
-    <UserContainer>
-      {isLoggedIn && (
-        <li>
-          <a href="#" onClick={() => props.logout()}>
-            Logout
-          </a>
-        </li>
-      )}
-      {isLoggedIn && (
-        <li>
-          <Link to="/posts/new">
-            <Button icon labelPosition="right">
-              <Icon name="plus" />
-              Post Rental
-            </Button>
-          </Link>
-        </li>
-      )}
+    <div>
+      {isLoggedIn ? (
+        <UserContainer>
+          <li>
+            <a href="#" onClick={() => props.logout()}>
+              Logout
+            </a>
+          </li>
 
-      <li>
-        <Link to="/login">Login</Link>
-      </li>
-      <li>
-        <Link to="/signup">Sign Up</Link>
-      </li>
-      <li>
-        <Link to="/posts">All Posts</Link>
-      </li>
-    </UserContainer>
+          <li>
+            <Link to="/posts/new">
+              <Button icon labelPosition="right">
+                <Icon name="plus" />
+                Post Rental
+              </Button>
+            </Link>
+          </li>
+          <li>
+            <Link to="/posts">All Posts</Link>
+          </li>
+        </UserContainer>
+      ) : (
+        <UserContainer>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+          <li>
+            <Link to="/posts">All Posts</Link>
+          </li>
+        </UserContainer>
+      )}
+    </div>
   )
 }
 
