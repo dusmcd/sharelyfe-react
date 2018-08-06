9bd83e5fcb08cd04181f3361a277b2d46461bc2c
Dustin McDowell <dustinmcdowell1989@gmail.com>
2018-08-06 09:30:17 -0500

    setting up user dashboard

---
+++ client/components/profile/user-dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+class UserDashboard extends React.Component {
+  componentDidMount() {
+    // fetch data
+  }
+  render() {
+    return <h1>User Dashboard</h1>
+  }
+}
+
+export default UserDashboard
