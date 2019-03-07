74acbe2a849e08bde3600cf2a9b546a69b4ddc20
Dustin McDowell <dustinmcdowell1989@gmail.com>
2019-03-06 19:28:04 -0600

    configuring environment for deployment to heroku

--- server/db/db.js
+++ server/db/db.js
@@ -2,9 +2,10 @@ const Sequelize = require('sequelize')
 
 const database =
   process.env.NODE_ENV === 'test' ? 'sharelyfe-test' : 'sharelyfe'
+const isLogging = process.env.NODE_ENV === 'production'
 
-const db = new Sequelize(`postgres://localhost:5432/${database}`, {
-  logging: false,
+const db = new Sequelize(process.env.DATABASE_URL + database, {
+  logging: isLogging,
 })
 
 module.exports = db
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV,
   entry: ['babel-polyfill', './client/index'],
   output: {
     path: path.resolve(__dirname, 'client', 'public'),
