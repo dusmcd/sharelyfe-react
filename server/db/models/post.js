cf7572558b57c80435afbc1beb64038219e09971
Dustin McDowell <dustinmcdowell1989@gmail.com>
2019-03-06 20:07:09 -0600

    made address fields optional

--- server/db/models/post.js
+++ server/db/models/post.js
@@ -1,7 +1,7 @@
-const db = require('../db')
-const Sequelize = require('sequelize')
-const Op = Sequelize.Op
-const axios = require('axios')
+const db = require('../db');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+const axios = require('axios');
 
 const Post = db.define('post', {
   imageUrl: {
@@ -28,33 +28,21 @@ const Post = db.define('post', {
   },
   address: {
     type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
   },
   city: {
     type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
   },
   state: {
     type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
   },
   zipcode: {
     type: Sequelize.STRING,
   },
-})
+});
 
 Post.filterPosts = function(queryParams) {
-  const queryString = `%${queryParams.search}%`
-  const { distanceRadius, origin } = queryParams
+  const queryString = `%${queryParams.search}%`;
+  const { distanceRadius, origin } = queryParams;
   return this.findAll({
     where: {
       [Op.or]: [
@@ -72,11 +60,11 @@ Post.filterPosts = function(queryParams) {
     },
   }).then(posts => {
     if (posts.length) {
-      return filterByDistance(posts, distanceRadius, origin)
+      return filterByDistance(posts, distanceRadius, origin);
     }
-    return []
-  })
-}
+    return [];
+  });
+};
 
 function filterByDistance(posts, distanceRadius, origin) {
   // use google maps api to filter by specified distance
@@ -84,22 +72,22 @@ function filterByDistance(posts, distanceRadius, origin) {
     .map(post => {
       return `${post.address.replace(/\s/g, '+')}+${post.city}+${post.state}+${
         post.zipcode
-      }`
+      }`;
     })
-    .join('|')
+    .join('|');
   const requestUrl = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin}&destinations=${destinations}&key=${
     process.env.GOOGLE_API_KEY
   }
-`
+`;
   return axios.get(requestUrl).then(res => {
-    const distanceResults = res.data.rows[0].elements
+    const distanceResults = res.data.rows[0].elements;
     const filteredPosts = posts.filter((post, i) => {
       if (distanceResults[i].distance.value <= +distanceRadius) {
-        return true
+        return true;
       }
-    })
-    return filteredPosts
-  })
+    });
+    return filteredPosts;
+  });
 }
 
 function formatDate(dateObj) {
@@ -107,67 +95,67 @@ function formatDate(dateObj) {
     month: dateObj.getMonth() + 1,
     day: dateObj.getDate(),
     year: dateObj.getFullYear(),
-  }
-  const { month, day, year } = dateDetails
-  return `${month}/${day}/${year}`
+  };
+  const { month, day, year } = dateDetails;
+  return `${month}/${day}/${year}`;
 }
 
 Post.prototype.formatBookings = function() {
   // puts the startDate and everything between the startDate and the endDate
   // in an object literal (Hash Table). Not including the endDate
-  const bookingMap = {}
-  const bookings = filterBookings(this.bookings)
+  const bookingMap = {};
+  const bookings = filterBookings(this.bookings);
   bookings.forEach(booking => {
-    let startDate = booking.startDate
-    bookingMap[formatDate(booking.startDate)] = true
-    const millisecondsBetween = booking.endDate.valueOf() - startDate.valueOf()
-    const daysBetween = millisecondsBetween / (1000 * 60 * 60 * 24) - 1
+    let startDate = booking.startDate;
+    bookingMap[formatDate(booking.startDate)] = true;
+    const millisecondsBetween = booking.endDate.valueOf() - startDate.valueOf();
+    const daysBetween = millisecondsBetween / (1000 * 60 * 60 * 24) - 1;
     for (let i = 1; i <= daysBetween; i++) {
       // there are 86,400,000 milliseconds in a day
-      const nextDateValue = startDate.valueOf() + 86400000
-      bookingMap[formatDate(new Date(nextDateValue))] = true
-      startDate = new Date(nextDateValue)
+      const nextDateValue = startDate.valueOf() + 86400000;
+      bookingMap[formatDate(new Date(nextDateValue))] = true;
+      startDate = new Date(nextDateValue);
     }
-  })
+  });
 
-  this.dataValues.datesReserved = bookingMap
-}
+  this.dataValues.datesReserved = bookingMap;
+};
 
 function filterBookings(bookings) {
   // filters bookings array to only include those that have a start date
   // that is after the current day. Uses binary search. When the middle
   // goes from greater than the current day to less than current day (in subsequent iteration)
   // then we can narrow down where to slice the original array
-  if (bookings.length < 5) return bookings
+  if (bookings.length < 5) return bookings;
 
   let leftBound = 0,
     wasGreater = false,
     rightBound = bookings.length - 1,
-    middle = Math.floor((rightBound - leftBound) / 2)
+    middle = Math.floor((rightBound - leftBound) / 2);
 
   while (rightBound !== middle) {
-    middle = leftBound + Math.floor((rightBound - leftBound) / 2)
+    middle = leftBound + Math.floor((rightBound - leftBound) / 2);
     if (middle >= 0 && bookings[middle].dataValues.startDate > Date.now()) {
-      rightBound = middle - 1
-      wasGreater = true
+      rightBound = middle - 1;
+      wasGreater = true;
     } else {
-      leftBound = middle + 1
+      leftBound = middle + 1;
       if (wasGreater) {
-        rightBound = bookings.length
-        return findCrossover(bookings.slice(leftBound, rightBound))
+        rightBound = bookings.length;
+        return findCrossover(bookings.slice(leftBound, rightBound));
       }
-      wasGreater = false
+      wasGreater = false;
     }
   }
-  return []
+  return [];
 }
 
 function findCrossover(filteredBookings) {
   for (let i = 0; i < filteredBookings.length; i++) {
     if (filteredBookings[i].dataValues.startDate > Date.now()) {
-      return filteredBookings.slice(i, filteredBookings.length)
+      return filteredBookings.slice(i, filteredBookings.length);
     }
   }
 }
 
-module.exports = Post
+module.exports = Post;
