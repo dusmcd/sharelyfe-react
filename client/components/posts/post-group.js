296dcae457de6d843cc6271ed35bd586b8aff929
Dustin McDowell <dustinmcdowell1989@gmail.com>
2019-06-10 11:32:26 -0500

    styling posts page

--- client/components/posts/post-group.js
+++ client/components/posts/post-group.js
@@ -1,16 +1,22 @@
 import React from 'react'
+import { Grid, Image } from 'semantic-ui-react'
 
 function PostGroup(props) {
   const { category, posts } = props
   if (!posts) return <div>LOADING...</div>
   return (
-    <div>
+    <div style={{ marginBottom: '20px' }}>
       <h2>{category.name}</h2>
-      <ul>
-        {posts.map(post => (
-          <li key={post.id}>{post.description}</li>
-        ))}
-      </ul>
+      <Grid divided="vertically">
+        <Grid.Row columns={3}>
+          {posts.map(post => (
+            <Grid.Column key={post.id}>
+              <Image src={post.imageUrl} rounded />
+              <h3>{post.title}</h3>
+            </Grid.Column>
+          ))}
+        </Grid.Row>
+      </Grid>
     </div>
   )
 }
--- server/api/categories.js
+++ server/api/categories.js
@@ -11,8 +11,25 @@ router.get('/posts', (req, res, next) => {
   return Category.findAll({
     include: [{ model: Post }],
   })
-    .then(categories => res.json(categories))
+    .then(categories => {
+      // going through categories and limiting posts to only 3
+      const limitedCategories = categories.map(category => {
+        let limitedPosts = []
+        if (category.posts) {
+          limitedPosts = category.dataValues.posts.slice(0, 3)
+        }
+        return { ...category.dataValues, posts: limitedPosts }
+      })
+      console.log('CATEGORIES:', limitedCategories)
+      res.json(limitedCategories)
+    })
     .catch(err => next(err))
+})
+
+router.get(':id/posts', (req, res, next) => {
+  return Category.findByPk(req.params.id, {
+    include: [{ model: Post }],
+  }).then(category => res.json(category))
 })
 
 module.exports = router
